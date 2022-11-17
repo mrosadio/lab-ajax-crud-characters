0f01bcaad1abf89fff7def04484c6c7ea5904d83
Micaela Rosadio <rosadio.karen@gmail.com>
2022-11-17 17:56:08 +0100

    Create API class and its methods to fetch all characters, fetch one, and delete one character

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,28 @@
 class APIHandler {
   constructor (baseUrl) {
-    this.BASE_URL = baseUrl;
+    this.BASE_URL = axios.create({
+      baseURL: baseUrl
+    });
   }
 
   getFullList () {
-
+    return this.BASE_URL.get("/characters");
   }
 
-  getOneRegister () {
-
+  getOneRegister = (characterId) => {
+    return this.BASE_URL.get(`/characters/${characterId}`);
   }
 
-  createOneRegister () {
-
+  createOneRegister = (characterInfo) => {
+    return this.BASE_URL.post("/characters", characterInfo);
   }
 
-  updateOneRegister () {
-
+  updateOneRegister = (characterId, characterInfo) => {
+    return this.BASE_URL.put(`/characters/${characterId}`, characterInfo);
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister = (characterId) => {
+    return this.BASE_URL.delete(`/characters/${characterId}`);
   }
 }
+
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,16 +1,50 @@
 const charactersAPI = new APIHandler('http://localhost:8000');
+//const router     = require("express").Router();
+//const ApiService = require("../services/api.services");
 
 window.addEventListener('load', () => {
   document.getElementById('fetch-all').addEventListener('click', function (event) {
-
+    charactersAPI.getFullList()
+    .then(characters => {
+      let cards = ``;
+      characters.data.forEach(character => {
+        cards += `
+        <div class="character-info">
+          <div class="name">${character.name}</div>
+          <div class="occupation">${character.occupation}</div>
+          <div class="cartoon">Is a Cartoon? ${character.cartoon ? "Yes" : "No"}</div>
+          <div class="weapon">${character.weapon}</div>
+        <div>`
+      });
+      const container     = document.createElement("div");
+      container.innerHTML = cards;
+      document.getElementsByClassName("characters-container")[0].appendChild(container);
+    })
+    .catch(err => console.log(err))
+    // Eliminate placeholder container
+    // Improve container
   });
 
   document.getElementById('fetch-one').addEventListener('click', function (event) {
-
+    let characterId = document.getElementById("select-character").value;
+    charactersAPI.getOneRegister(characterId)
+    .then(character => {
+        let data = character.data;
+        document.getElementById("name-char").innerText    = data.name;
+        document.getElementById("occup-char").innerText   = data.occupation;
+        document.getElementById("cartoon-char").innerText = data.cartoon;
+        document.getElementById("weapon-char").innerText  = data.weapon;
+    })
+    .catch(err => console.log(err))
   });
 
   document.getElementById('delete-one').addEventListener('click', function (event) {
-
+    let characterId = document.getElementById("select-character-delete").value;
+    charactersAPI.deleteOneRegister(characterId)
+    .then(character => {
+      console.log("log", character)
+    })
+    .catch(err => console.log(err))
   });
 
   document.getElementById('edit-character-form').addEventListener('submit', function (event) {
